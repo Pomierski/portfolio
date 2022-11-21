0957f56a10cdc0054e0dd0f3aa2702cd1528ad77
Piotr Pomierski <pomierskipiotr@gmail.com>
2022-11-21 19:46:23 +0100

    feat: add language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import * as aboutTranslations from "./sections/About/translations/index.json";
 import * as contactTranslations from "./sections/ContactCard/translations/index.json";
@@ -7,6 +8,7 @@ import * as projectsTranslations from "./sections/Projects/translations/index.js
 import * as skillsTranslations from "./sections/Skills/translations/index.json";
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     fallbackLng: "en", // use en if detected lng is not available
